2f1f9007cba178d6a218d21a158c70355de66670
pranshu <pranshugautam1998@gmail.com>
2022-12-10 18:17:17 +0530

    commit 9

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,8 +14,8 @@ public class Main {
     }
   public static void main(String[] args) {
     B b = new B();
-    //System.out.println(b.meth());
-    b.meth();
+    System.out.println(b.meth());
+    //b.meth();
     //b.meth();
   }
 }
