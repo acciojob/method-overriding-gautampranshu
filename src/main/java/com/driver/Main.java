03ab393c1cef813e7fc847961eebf3116a8746da
pranshu <pranshugautam1998@gmail.com>
2022-12-10 18:09:49 +0530

    commit 6

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,17 +6,13 @@ package com.driver;
   }
 }
  class B extends A{
-  public String meth()
-  {
-    return "Method is overridden in Extendend class B";
-  }
+//  public String meth()
+//  {
+//    return "Method is overridden in Extendend class B";
+//  }
 }
 public class Main {
   public static void main(String[] args) {
-    A a = new B();
-    //System.out.println(b.meth());
-    //System.out.println(a.meth());
-    a.meth();
     B b = new B();
     //System.out.println(b.meth());
     b.meth();
